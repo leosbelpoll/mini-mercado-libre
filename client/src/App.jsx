cce774cf22c19d60a0f0cc63f433e78b5a034d31
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-03-05 21:45:16 -0300

    Create Client basic structure

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles/App.scss";
+
+const App = () => (
+    <div className="container">
+        <h1>Hello World :)</h1>
+    </div>
+);
+
+export default App;
