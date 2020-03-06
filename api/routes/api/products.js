7e4710e7e316900069bfdb2395ee698ac86d9677
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-03-05 21:29:55 -0300

    Add products cache

--- api/routes/api/products.js
+++ api/routes/api/products.js
@@ -1,9 +1,13 @@
 import express from "express";
+import apicache from "apicache";
 
 import product from "../../src/mocks/product";
 import products from "../../src/mocks/products";
 
 const router = express.Router();
+const cacheMiddleware = apicache.middleware;
+
+router.use(cacheMiddleware(`${process.env.CACHE_MILISECONDS || 60000} ms`));
 
 router.get("/", (req, res) => {
     const { q } = req.query;
