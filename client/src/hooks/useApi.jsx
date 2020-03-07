7343a707c332c958b96c3bdefe8b7840099c026a
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-03-07 14:37:11 -0300

    Add some 'Integrity' to API response

--- client/src/hooks/useApi.jsx
+++ client/src/hooks/useApi.jsx
@@ -1,13 +1,24 @@
 import useFetch from "./useFetch";
 
 const useApi = (url, method = "GET", body = null, header = []) => {
-    return useFetch(
+    const fecthApi = useFetch(
         `${process.env.REACT_APP_API_URL ||
             "http://localhost:3001"}/api/${url}`,
         method,
         body,
         header
     );
+    const { value } = fecthApi;
+    if (
+        value !== null &&
+        (value.author.name !==
+            (process.env.REACT_APP_AUTHOR_NAME || "Leosbel") ||
+            value.author.lastname !==
+                (process.env.REACT_APP_AUTHOR_LASTNAME || "Poll"))
+    ) {
+        throw new Error("Unauthorized sign");
+    }
+    return fecthApi;
 };
 
 export default useApi;
