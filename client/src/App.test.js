e1c1fc895e2d12a199c1f8405504eab2756b7c4d
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-03-09 08:26:51 -0300

    Client > Test > Modify App tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,19 +1,14 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import App from "./App";
-import { HelmetProvider } from "react-helmet-async";
-import Header from "./components/parts/Header";
-import MetaTags from "./components/parts/MetaTags";
-
-it("renders without crashing", () => {
-    shallow(<App />);
-});
 
 describe("App", () => {
-    const wrapper = mount(<App />);
-    it("Case: Verify component elements", () => {
-        expect(wrapper.find(HelmetProvider).length).toEqual(1);
-        expect(wrapper.find(MetaTags).length).toEqual(1);
-        expect(wrapper.find(Header).length).toEqual(1);
-    });
+  it("Case: Verify component elements", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toBeTruthy();
+    expect(wrapper.exists("HelmetProvider")).toBeTruthy();
+    expect(wrapper.exists("MetaTags")).toBeTruthy();
+    expect(wrapper.exists("Header")).toBeTruthy();
+    expect(wrapper.find("Route").length).toEqual(4);
+  });
 });
