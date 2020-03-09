ff1b79329dcbcd193d05bee0b8b6555ae105ef06
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-03-09 03:00:03 -0300

    Fix class to className in EmptyList component

--- client/src/components/parts/EmptyList.jsx
+++ client/src/components/parts/EmptyList.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const EmptyList = () => (
     <section className="EmptyList__section">
-        <div class="icon">
+        <div className="icon">
             <img src="/images/glass.svg" alt="Search icon" />
         </div>
-        <div class="info">
-            <h3 class="header__title">
+        <div className="info">
+            <h3 className="header__title">
                 No hay elementos que coincidan con tu búsqueda.
             </h3>
-            <ul class="links__list">
+            <ul className="links__list">
                 <li>Revisa la ortografía de la palabra.</li>
                 <li>Utiliza palabras más genéricas o menos palabras.</li>
             </ul>
